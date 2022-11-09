c6136ed2ddb01d5c61d8f2af4e21b09e3e4d622e
Ashraf59 <ashrafams59b@gmail.com>
2022-11-09 20:50:50 +0600

    My review api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,13 +36,48 @@ async function run(){
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
-        })
+        });
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = {_id: ObjectId(id)};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        });
 
         app.get('/allservices', async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const allservices = await cursor.toArray();
             res.send(allservices);
+        });
+
+        // create reviews API
+        app.post('/reviews', async(req, res) => {
+            const query = req.body;
+            const result = await reviewCollection.insertOne(query);
+            res.send(result);
+        });
+
+        app.get('/reviews', async(req, res) => {
+            const query = {}
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.get('/review', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query)
+            const review = await cursor.toArray()
+            res.send(review)
         })
     }
     finally{
